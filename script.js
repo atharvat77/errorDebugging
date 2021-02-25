acfe84c0f731f811f9d9a0bf099e7d0f258951c6
Atharvat77 <Atharvat77@gmail.com>
2021-02-25 09:43:50 +0530

    error

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var bgImg, x1 = 0, x2, scrollSpeed = 4, mario, brickGroup, coinsGroup, coinImage, coinSound, Score = 0, diamondGroup, diamondImage, bgMusic, diamondSound, mushRunning, obstacleGroup, die,  
-turtleImage, marioDead, playing=0,gameState = "PLAY"; //declaring the variables
+var bgImg, x1 = 0, x2, scrollSpeed = 4, mario, brickGroup, 
+coinsGroup, coinImage, coinSound, Score = 0, diamondGroup, diamondImage, 
+bgMusic, diamondSound, mushRunning, obstacleGroup, die,  
+turtleImage, playing=0,gameState = "PLAY"; //declaring the variables
+var mario_running,mario_dead;
 
 
 function preload() {
-  mario_running = loadAnimation('images/mar1.png', 'images/mar2.png', 'images/mar3.png', 'images/mar4.png', 'images/mar5.png', 'images/mar6.png', 'images/mar7.png'); //loading the animation of the mario sprite 
+  mario_running = loadAnimation('images/mar1.png', 'images/mar2.png', 'images/mar3.png', 'images/mar4.png', 'images/mar5.png', 'images/mar6.png', 'images/mar7.png'); //loading the  of the mario sprite 
 
   mushRunning = loadAnimation("images/mush1.png", "images/mush2.png", "images/mush3.png", "images/mush4.png", "images/mush5.png", "images/mush6.png"); //loading the animtion for enemy 1 - mush
 
@@ -13,25 +16,25 @@ function preload() {
   coinSound = loadSound('sounds/coinSound.mp3'); //loading the coin sound
   die = loadSound('sounds/dieSound.mp3'); //loading the sound on death of mario
 
-  marioDead = loadImage('images/dead.png') //mario's death image
-  diamondSound = loadSound('sounds/diamond.mp3'); //loading the diamond sound
+  mario_dead = loadAnimation('images/dead.png'); //mario's death image
 
-  coinImage = loadAnimation('images/con1.png', 'images/con2.png', 'images/con3.png', 'images/con4.png', 'images/con5.png', 'images/con6.png'); //loading the animation of coin
 
-  diamondImage = loadImage("images/diamond.png"); //loading the image of the diamond
-  turtleImage = loadAnimation("images/tur1.png","images/tur2.png","images/tur3.png","images/tur4.png","images/tur5.png"); //loading the animation of enemy 2 - turtle
+  coinImage = loadAnimation('images/con1.png', 'images/con2.png', 'images/con3.png', 'images/con4.png', 'images/con5.png', 'images/con6.png'); //loading the  of coin
 
-  bgMusic = loadSound('sounds/bg.mp3') //loading the background music
+  turtleImage = loadAnimation("images/tur1.png","images/tur2.png","images/tur3.png","images/tur4.png","images/tur5.png"); //loading the  of enemy 2 - turtle
+
+  //bgMusic = loadSound('sounds/bg.mp3') //loading the background music
 }
 
 function setup() {
   gameScreen = createCanvas(800, 400); //creating the game screen
-  bgMusic.play();
-  gameScreen.position(285, 250); //positioning the screen
+  //bgMusic.play();
+  //gameScreen.position(285, 250); //positioning the screen
   x2 = width; // taking the width in x2
 
   mario = createSprite(100, 345, 20, 50); //creating a sprie of mario
-  mario.addAnimation("running", mario_running); //adding the preloaded animation to mario
+   //adding the preloaded  to mario
+  
   mario.scale = 0.15; //scaling the sprite of mario to 0.15
 
   brickGroup = new Group(); //craeting a new brick group
@@ -42,6 +45,8 @@ function setup() {
 
   obstacleGroup = new Group(); // creating a new mush group
 
+  mario.addAnimation("running", mario_running);
+  mario.addAnimation("dead",mario_dead);
 
 
   ground = createSprite(100, 345, 400, 10); // creating the ground for mario
@@ -50,25 +55,22 @@ function setup() {
 
 
 function draw() {
+
+
   if(gameState == "PLAY"){
-  // if(playing == 0){
-  //   bgMusic.play();
-  //   playing =1 ;
-  // }
+   
+ 
   image(bgImg, x1, 0, width, height); // displaying the background image
   image(bgImg, x2, 0, width, height);
 
-  generateBricks();
-  generateCoins();
-  generateDiamonds();
-  generateObstacles();
-  // bgMusic.play();
+  
+
   if (keyDown("space")) { //Checking if the key down is specabar
     mario.velocityY = -13; //then mario gets an upward velocuiy of 13
   }
 
   mario.velocityY += 0.5;//and we constantly make him to fall down due to a downward velocity
-  mario.collide(ground); // and then we make him to collide with the ground such taht it doesnot go beyond the ground
+   // and then we make him to collide with the ground such taht it doesnot go beyond the ground
   x1 -= scrollSpeed; // scroll speed for the height of the bg iamge
   x2 -= scrollSpeed; // scroll speed for the width of the bg image
 
@@ -83,6 +85,7 @@ function draw() {
     x2 = width;
   }
 
+  generateBricks();
   //Checking for collision wiht bricks
   for (var i = 0; i < (brickGroup).length; i++) {
     var temp = (brickGroup).get(i);
@@ -91,6 +94,8 @@ function draw() {
       mario.collide(temp);
     }
   }
+
+  generateCoins();
 //Checking for collision with coins and score counter
   for (var j = 0; j < (coinsGroup).length; j++) {
     var temp2 = (coinsGroup).get(j);
@@ -102,25 +107,35 @@ function draw() {
       Score += 1;
     }
   }
-//Checking for collision with diamonds and score counter
-  for (var k = 0; k < (diamondGroup).length; k++) {
-    var temp3 = (diamondGroup).get(k);
 
-    if (temp3.isTouching(mario)) {
-      diamondSound.play();
-      temp3.destroy();
-      temp3 = null;
-      Score += 10;
-    }
-
-  }
+ 
 
   
+  
+  
+  generateObstacles();
+  
     if (obstacleGroup.isTouching(mario)) {
+      console.log("Dead");
       gameState="END";
-      die.play();
+      //die.play();
     }
   }
+
+  else if(gameState=="END")
+  {
+    
+    scrollSpeed=0;
+    mario.velocityX=0;
+    mario.velocityY=0;
+    obstacleGroup.setVelocityXEach(0);
+    coinsGroup.setVelocityXEach(0);
+    diamondGroup.setVelocityXEach(0);
+    brickGroup.setVelocityXEach(0);
+    mario.changeAnimation("dead",mario_dead);
+    mario.setCollider("rectangle",0,0,300,10);
+  }
+  mario.collide(ground);
   
 //Calling all the required functions 
   drawSprites(); //draws all the sprite
@@ -129,9 +144,7 @@ function draw() {
   text("Score:" + Score, 600, 20); // score at600x and 20y
 }
 
-function random(min, max) { // a function to generate random numbers 
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+
 
 // Function to generate Bricks
 function generateBricks() {
@@ -156,23 +169,13 @@ function generateCoins() {
     coinsGroup.add(coin);
   }
 }
-//Fnction to generate diamonds
-function generateDiamonds() {
-  if (Score % 10 == 0 && frameCount % 100 == 0) {
-    var diamonds = createSprite(1200, random(100, 300), 40, 10);
-    diamonds.addImage(diamondImage);
-    diamonds.velocityX = -3;
-    diamonds.scale = 0.2;
-    diamondGroup.add(diamonds);
-  }
 
-}
-//Function to generate Mush
+
 function generateObstacles() {
   if (frameCount % 100 == 0) {
     var obstacles = createSprite(random(1000, 1400), 315, 40, 40);
-    var choice= round
-    (random(0,1));
+    var choice= round(random(0,1));
+
     if(choice === 0){
     obstacles.addAnimation("mush", mushRunning);
     }
